a697373680babd6ed7bfa8c674dc888db71c7067
JacquesFernandes <jacques.fernandes.1337@gmail.com>
2017-01-30 13:09:25 +0530

    added mongoose-schema object to player class

--- resources.js
+++ resources.js
@@ -4,11 +4,6 @@ function Weapon(dmg, rate, name, level)
   this.dmg = (dmg === undefined)? 1 : dmg ; // damage per hit
   this.rate = (rate === undefined)? 1 : rate; // Rate of fire
   this.name = (name === undefined)? "Some Gun" : name; // Name of Gun
-
-  this.fire = function()
-  {
-    console.log("FIRING "+this.name);
-  };
 };
 
 function Player(userid, player_name)
@@ -25,12 +20,38 @@ function Player(userid, player_name)
   this.weapons.heavy = new Weapon(20,5,"Heavy Gun");
   this.weapons.sniper = new Weapon(30,1,"Sniper Guns");
 
-  /* METHODS */
+  this.schema = {
+    userid: String,
+    username: String,
+    hp: Number,
+    movement_speed: Number,
+    exp: Number,
+    level: Number,
+    weapons: {
+      light: {
+        level: Number,
+        rate: Number,
+        name: String
+      },
+      heavy: {
+        level: Number,
+        rate: Number,
+        name: String
+      },
+      sniper: {
+        level: Number,
+        rate: Number,
+        name: String
+      }
+    }
+  };
+  /* METHODS
   this.getAttributeJSON = function()
   {
-    ret = {"userid":this.userid, "username":this.name, "hp":this.hp, "movement":this.movement_speed};
+    ret = {"userid":this.userid, "username":this.name, "hp":this.hp, "movement_speed":this.movement_speed};
     return(ret);
   };
+  */
 };
 
 module.exports.Player = Player;
