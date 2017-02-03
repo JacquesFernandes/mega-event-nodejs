ec8ecda8781612077aa9cc7cc005048dee2ffcaf
JacquesFernandes <jacques.fernandes.1337@gmail.com>
2017-02-03 09:28:00 +0530

    added player lobby files

---
+++ routes/lobby.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+var mongoose = require("mongoose");
+
+var player_lobby = [];
+/* ROUTES */
+router.get("/",function(req,res)
+{
+    if (req.SomeCookie.username)
+    {
+        player_lobby.push(req.SomeCookie.username);
+    }
+    else
+    {
+        res.redirect("/");
+    }
+    res.render("lobby");
+});
+
+
+module.exports = router;
