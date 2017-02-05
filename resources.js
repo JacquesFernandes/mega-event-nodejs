50f9141fef54fcf78bf795b443f3c9b73e575be1
JacquesFernandes <jacques.fernandes.1337@gmail.com>
2017-02-05 21:28:11 +0530

    removed "userid" from player object (since username is unique)

--- resources.js
+++ resources.js
@@ -15,15 +15,15 @@ module.exports.MiscFunctions = MiscFunctions;
 
 function Weapon(dmg, rate, name, level)
 {
-  this.level = (level === undefined)? 1 : level ; //level of the gun
+  this.level = (level === undefined)? 0 : level ; //level of the gun
   this.dmg = (dmg === undefined)? 1 : dmg ; // damage per hit
   this.rate = (rate === undefined)? 1 : rate; // Rate of fire
   this.name = (name === undefined)? "Some Gun" : name; // Name of Gun
 };
 
-function Player(userid, player_name)
+function Player(player_name)
 {
-  this.userid = (userid === undefined)? "LOLWUT" : userid;
+  //this.userid = (userid === undefined)? "LOLWUT" : userid;
   this.name = (player_name === undefined)? "N00B" : player_name;
   this.hp = 100;
   this.movement_speed = 10;
@@ -36,7 +36,7 @@ function Player(userid, player_name)
   this.weapons.sniper = new Weapon(30,1,"Sniper Guns");
 
   this.schema = {
-    userid: String,
+    //userid: String,
     username: String,
     hp: Number,
     movement_speed: Number,
@@ -67,7 +67,7 @@ function Player(userid, player_name)
   this.toJSON = function()
   {
     ret = {
-      userid: this.userid,
+      //userid: this.userid,
       username: this.name,
       hp: this.hp,
       movement_speed: this.movement_speed,
