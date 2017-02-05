6bb6815a68adda5aa8599c2c6aa763a14647456f
JacquesFernandes <jacques.fernandes.1337@gmail.com>
2017-02-05 21:28:39 +0530

    Made a Schemas file (Contains used schemas)

---
+++ Schemas.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+
+var weaponTypeSchema = mongoose.Schema({
+  level: Number,
+  rate: Number,
+  name: String,
+  dmg: Number
+});
+var weaponSchema = mongoose.Schema({
+  light:{
+    type: weaponTypeSchema
+  },
+  heavy:{
+    type: weaponTypeSchema
+  },
+  sniper:{
+    type:weaponTypeSchema
+  }
+});
+var playerSchema = mongoose.Schema({
+  username: String,
+  hp: Number,
+  movement_speed: Number,
+  exp: Number,
+  level: Number,
+  weapons: {
+      type: weaponSchema
+  }
+});
+
+var PlayerModel = mongoose.model("players",playerSchema);
+
+module.exports.weaponTypeSchema = weaponTypeSchema;
+module.exports.weaponSchema = weaponSchema;
+module.exports.playerSchema = playerSchema;
+module.exports.PlayerModel = PlayerModel;
